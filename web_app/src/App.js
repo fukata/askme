cba40abdf262a01d2d3e9023d9468c54d8c58af0
fukata <tatsuya.fukata@gmail.com>
2017-11-26 19:57:28 +0700

    use Link module

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import QuestionNew from './QuestionNew';
 import logo from './logo.svg';
@@ -30,7 +30,7 @@ class App extends Component {
         <div>
           <p>Hello, {this.state.config.user.username}, <a href={process.env.REACT_APP_API_ENDPOINT + "/logout"}>Logout</a></p>
           <p>Your question form url is below</p>
-          <p><a href={'/q/' + this.state.config.user.username} target="_blank">Question Form</a></p>
+          <p><Link to={`/q/${this.state.config.user.username}`}>Question Form</Link></p>
         </div>
       );
     } else {
